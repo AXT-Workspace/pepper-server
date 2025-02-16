30d0dcc17f3b79c93076629d5992025ac4b59db1
Tayomide <tibrahim@hawk.iit.edu>
2025-02-16 17:04:08 -0600

    Make obscure path

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
   return {"message": "Hello World"}
+
+@app.get("/I/don't/care")
+async def i_dont_care():
+  return {"message": "I don't care", "mood": "happy"}
