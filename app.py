c172fed1a61a94f92436122b5fd571fc827163b5
Tayomide <tibrahim@hawk.iit.edu>
2025-02-20 16:25:45 -0600

    Add endpoint

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from models.chat import Chat
+
 app = FastAPI()
 
 @app.get("/")
@@ -9,3 +11,9 @@ async def root():
 @app.get("/I/don't/care")
 async def i_dont_care():
   return {"message": "I don't care", "mood": "happy"}
+
+@app.post("/chat")
+async def chat(chat: Chat):
+  print(chat)
+
+  return {"response": "Loud and clear", "mood": "happy"}
