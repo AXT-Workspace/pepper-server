b7cc008e372be1c65b22ac843e1bfbb6940079d8
Tayomide <tibrahim@hawk.iit.edu>
2025-02-23 00:16:57 -0600

    Strictly type endpoints

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from models.chat import Chat
+from models.chat import Chat, ChatResponse
+from models.root import RootResponse
+from models.i_dont_care import IDontCareResponse
 from dotenv import load_dotenv
 import os
 from google import genai
@@ -10,20 +12,18 @@ app = FastAPI()
 client = genai.Client(api_key = os.getenv("GOOGLE_API_KEY"))
 
 @app.get("/")
-async def root():
-  return {"message": "Hello World"}
+async def root() -> RootResponse:
+  return RootResponse(message="Hello World")
 
 @app.get("/I/don't/care")
-async def i_dont_care():
-  return {"message": "I don't care", "mood": "happy"}
+async def i_dont_care() -> IDontCareResponse:
+  return IDontCareResponse(message="I don't care", mood="happy")
 
 @app.post("/chat")
-async def chat(chat: Chat):
-  print(chat)
-  
+async def chat(chat: Chat) -> ChatResponse:
   # client = genai.Client(api_key = os.getenv("GOOGLE_API_KEY"))
   response = client.models.generate_content(
-      model="gemini-2.0-flash-lite-preview-02-05", contents=chat.message
+    model="gemini-2.0-flash-lite-preview-02-05", contents=chat.message
   )
   print(response.text)
-  return {"response": "Loud and clear", "mood": "happy"}
+  return ChatResponse(response=response.text, mood="happy")
